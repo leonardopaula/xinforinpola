e4c92ff452f6d7ff4ef5c36edd7cd41607df365e
Leonardo S. Paula <leonardopaula@gmail.com>
2024-08-29 23:51:32 -0300

    Update error list

--- www/app/Enums/ErrorCodes.php
+++ www/app/Enums/ErrorCodes.php
@@ -8,6 +8,13 @@ enum ErrorCodes: int
 {
     case TRANSFER_AUTH_ERROR = 10001;
     case TRANSFER_UNAUTHORIZED_TRANSFER = 10002;
+    case TRANSFER_PAYER_NOT_FOUND = 10003;
+    case TRANSFER_PAYEE_NOT_FOUND = 10004;
+    case TRANSFER_TYPE_CONFIGURATION_NOT_FOUND = 10005;
+    case TRANSFER_INVALID_USER_TYPE_TRANSACTION = 10006;
+    case TRANSACTION_PAYER_WALLET_NOT_FOUND = 10007;
+    case TRANSACTION_PAYEE_WALLET_NOT_FOUND = 10008;
+    case TRANSACTION_INSUFFICIENT_BALANCE = 10009;
 
     public function info(): array
     {
@@ -24,7 +31,35 @@ enum ErrorCodes: int
             ErrorCodes::TRANSFER_UNAUTHORIZED_TRANSFER => [
                 'message' => 'Authorization service returns unauthorized transfer',
                 'httpCode' => Response::HTTP_BAD_REQUEST
-            ]
+            ],
+            ErrorCodes::TRANSFER_PAYER_NOT_FOUND => [
+                'message' => 'Payer not found',
+                'httpCode' => Response::HTTP_BAD_REQUEST
+            ],
+            ErrorCodes::TRANSFER_PAYEE_NOT_FOUND => [
+                'message' => 'Payee not found',
+                'httpCode' => Response::HTTP_BAD_REQUEST
+            ],
+            ErrorCodes::TRANSFER_TYPE_CONFIGURATION_NOT_FOUND => [
+                'message' => 'Configuration payee type x payer type not found',
+                'httpCode' => Response::HTTP_INTERNAL_SERVER_ERROR
+            ],
+            ErrorCodes::TRANSFER_INVALID_USER_TYPE_TRANSACTION => [
+                'message' => 'Payer incompatible with payee',
+                'httpCode' => Response::HTTP_INTERNAL_SERVER_ERROR
+            ],
+            ErrorCodes::TRANSACTION_PAYER_WALLET_NOT_FOUND => [
+                'message' => 'Payer wallet not found',
+                'httpCode' => Response::HTTP_INTERNAL_SERVER_ERROR
+            ],
+            ErrorCodes::TRANSACTION_PAYEE_WALLET_NOT_FOUND => [
+                'message' => 'Payee wallet not found',
+                'httpCode' => Response::HTTP_INTERNAL_SERVER_ERROR
+            ],
+            ErrorCodes::TRANSACTION_INSUFFICIENT_BALANCE => [
+                'message' => 'Insufficient balance',
+                'httpCode' => Response::HTTP_BAD_REQUEST
+            ],
         };
     }
 }
